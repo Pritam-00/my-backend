0e4d3f2c304544355b78a844aeeb142401e4a259
Pritam-00 <pritambera11235@gmail.com>
2024-03-16 07:33:01 +0530

    a lot is added

--- src/index.js
+++ src/index.js
@@ -1,85 +1,20 @@
-import express from 'express';
-import cors from 'cors';
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
 
-const app = express()
-const port = process.env.PORT || 3000
-let ranisuiBanner, bhiguLakeBanner, hamptaPassBanner, yunamBannerPhoto
-
-app.use(cors())
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+dotenv.config({
+  path: './env'
 })
 
-app.get('/create', (req, res) => {
-    res.send('new trek create')
-  })
 
-app.get('/login', (req, res) => {
-    res.send('<h1>no login page here</h1>')
-  })
-
-app.get('/api/mockdata', (req, res) => {
-  const mockdata = [
-    {
-      trekIndex: 101 ,
-      trekImg: ranisuiBanner,
-      trekName: 'Ranisui Lake Trek',
-      trekState: 'Himachal',
-      trekDescription: 'At the back yard of Manali, offers a serene summit (13, 220ft) and a alpine lake (12,385ft). A moderate trek with a 360 degree view of  stunning peaks, best explored from May to December.',
-      trekDistance: '25km',
-      trekDuration: '4Days',
-      trekDifficulty: '0.7',
-      trekCost: '9,500',
-      trekInfo: 'stay included',
-      trekLink: '/ranisui-lake-trek',
-    }  ,
-  
-    {
-      trekIndex: 102 ,
-      trekImg: bhiguLakeBanner,
-      trekName: 'Bhrigu Lake Trek',
-      trekState: 'Himachal',
-      trekDescription: 'Anali,220ft) and a alpine lake (12,385ft). A moderate trek with a 360 degree view of  stunning peaks, best explored from May to December.',
-      trekDistance: '25km',
-      trekDuration: '4Days',
-      trekDifficulty: '0.6',
-      trekCost: '9,500',
-      trekInfo: 'transport included',
-      trekLink: '/bhrigu-lake-trek',
-    }  ,
-  
-    {
-      trekIndex: 103 ,
-      trekImg: hamptaPassBanner,
-      trekName: 'Hampta Pass Trek',
-      trekState: 'Himachal',
-      trekDescription: 'At the back yard of Manali, offers a serene summit (13, 220ft) and a alpine lake (12,385ft). A moderate trek with a 360 degree view of  stunning peaks, best explored from May to December.',
-      trekDistance: '35km',
-      trekDuration: '5Days',
-      trekDifficulty: '0.5',
-      trekCost: '10,500',
-      trekInfo: 'transport included',
-      trekLink: 'hampta-pass-trek',
-    }  ,
-  
-    {
-        trekIndex: 104 ,
-        trekImg: yunamBannerPhoto,
-        trekName: 'Yunam Peak Expedition',
-        trekState: 'Himachal',
-        trekDescription: 'Anali,220ft) and a alpine lake (12,385ft). A moderate trek with a 360 degree view of  stunning peaks, best explored from May to December.',
-        trekDistance: '40km',
-        trekDuration: '9Days',
-        trekDifficulty: '6',
-        trekCost: '25,000',
-        trekInfo: 'transport included',
-        trekLink: '/yunam-peak-expedition', 
-    }
-  ];
-  res.send(mockdata);
+connectDB()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`server is running on port ${process.env.PORT}`)
+  });
+})
+.catch((error) => {
+  console.log("MONGODB connection failed ! ", error);
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+
